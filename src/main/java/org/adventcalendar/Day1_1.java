c0ce6fede2808c1e44d206a8e0e3523311b88b9e
RaulPincotan <72758658+RaulPincotan@users.noreply.github.com>
2023-12-02 11:58:04 +0200

    coordinates part 2

--- src/main/java/org/adventcalendar/Day1_1.java
+++ src/main/java/org/adventcalendar/Day1_1.java
@@ -3,12 +3,13 @@ package org.adventcalendar;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Map;
 
 import static java.nio.file.Files.lines;
 
 public class Day1_1 {
 
-    private static final String SOURCE_FILE = "src/main/resources/coordinates.txt";
+    private static final String SOURCE_FILE = "src/main/resources/coordinates2.txt";
 
     public static void main(String[] args) {
         System.out.println("calibrateCoordinates() = " + calibrateCoordinates());
@@ -25,20 +26,52 @@ public class Day1_1 {
     private static int getCoordinates(String row) {
         int firstNumber = 0;
         int lastNumber = 0;
+        String withDigits = convertLettersToDigits(row);
 
-        for (int i = 0, j = row.length() - 1; i < row.length(); i++) {
-            boolean isDigitFirst = Character.isDigit(row.charAt(i));
-            boolean isDigitLast = Character.isDigit(row.charAt(j));
-            if (isDigitFirst && firstNumber == 0) {
-                firstNumber = Integer.parseInt(row.substring(i, i + 1));
+        for (int i = 0, j = withDigits.length() - 1; i < withDigits.length(); i++) {
+            if (firstNumber == 0) {
+                firstNumber = Integer.parseInt(withDigits.substring(i, i + 1));
             }
-            if (isDigitLast && lastNumber == 0) {
-                lastNumber = Integer.parseInt(row.substring(j, j + 1));
+            if (lastNumber == 0) {
+                lastNumber = Integer.parseInt(withDigits.substring(j, j + 1));
             }
             j--;
         }
 
         return firstNumber * 10 + lastNumber;
+    }
+
+    private static String convertLettersToDigits(String row) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < row.length(); i++) {
+            String key = row.substring(i, i + 1);
+            for (int j = i + 1; j < row.length(); j++) {
+                key += row.substring(j, j + 1);
+                if (mapToDigit().containsKey(key)) {
+                    result.append(mapToDigit().get(key));
+                    break;
+                }
+            }
+            if (Character.isDigit(row.charAt(i))) {
+                result.append(row.charAt(i));
+            }
+        }
+        return result.toString();
+    }
+
+
+    private static Map<String, Integer> mapToDigit() {
+        return Map.of(
+                "one", 1,
+                "two", 2,
+                "three", 3,
+                "four", 4,
+                "five", 5,
+                "six", 6,
+                "seven", 7,
+                "eight", 8,
+                "nine", 9
+        );
     }
 
 
